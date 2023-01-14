edd054b14feab3820cb313649e21ea951a26820b
A. Hinton <7063649+ahcode0919@users.noreply.github.com>
2023-01-13 20:34:05 -0800

    Add Contains and Random String helpers

--- Sources/Fast/Extensions/String+Extension.swift
+++ Sources/Fast/Extensions/String+Extension.swift
@@ -8,6 +8,21 @@
 import Foundation
 
 public extension String {
+    static func random(length: Int) -> String {
+        let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        return String((0..<length).map { _ in letters.randomElement()! })
+    }
+
+    func contains(_ type: NSTextCheckingResult.CheckingType) -> Bool {
+        let range = NSRange(0..<self.count)
+        let types: NSTextCheckingResult.CheckingType = [type]
+        guard let dataDetector = try? NSDataDetector(types: types.rawValue),
+              let match = dataDetector.firstMatch(in: self, options: [], range: range),
+              match.resultType == type else {
+            return false
+        }
+        return true
+    }
     
     /// Removes whitespace and Newlines from the beginning and ending of the String
     /// - Returns: String without whitespace at the beginning or end
--- Tests/FastTests/ExtensionTests/String+ExtensionTests.swift
+++ Tests/FastTests/ExtensionTests/String+ExtensionTests.swift
@@ -9,6 +9,19 @@ final class StringExtensionTests: XCTestCase {
         XCTAssertEqual("test \n\r  ".chomp(), "test")
         XCTAssertEqual("test    \n ".chomp(), "test")
     }
+    
+    func testContainsPattern() {
+        let phoneNumber = "312-678-2234"
+        XCTAssertTrue(phoneNumber.contains(.phoneNumber))
+        XCTAssertFalse(phoneNumber.contains(.link))
+    }
+    
+    func testRandom() {
+        var randomStrings = Set<String>()
+        for _ in 0..<100 {
+            XCTAssertTrue(randomStrings.insert(String.random(length: 100)).inserted)
+        }
+    }
 
     func testWithQuotes() {
         XCTAssertEqual("foo".withQuotes(), "\"foo\"")
