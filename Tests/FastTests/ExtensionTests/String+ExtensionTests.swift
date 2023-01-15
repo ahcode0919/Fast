bef6a1bd7406e71abaed758485847c95f99832d1
A. Hinton <7063649+ahcode0919@users.noreply.github.com>
2023-01-15 12:27:17 -0800

    Add scanner extraction helpers

--- Tests/FastTests/ExtensionTests/String+ExtensionTests.swift
+++ Tests/FastTests/ExtensionTests/String+ExtensionTests.swift
@@ -16,6 +16,16 @@ final class StringExtensionTests: XCTestCase {
         XCTAssertFalse(phoneNumber.contains(.link))
     }
     
+    func testExtractDoubles() {
+        let testString = "23 L00.456 45.4KG 5,6.7"
+        XCTAssertEqual(testString.extractDoubles(), [23, 0.456, 45.4, 5, 6.7])
+    }
+    
+    func testExtractInts() {
+        let testString = "23 L456 45KG 5,6.7"
+        XCTAssertEqual(testString.extractInts(), [23, 456, 45, 5, 6, 7])
+    }
+    
     func testRandom() {
         var randomStrings = Set<String>()
         for _ in 0..<100 {
